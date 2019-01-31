d7603684a5acc38cb7ada5898e0eed731375f44a
mobapp05 <mobapp05@dt-c205-03.local>
2019-01-31 14:27:41 +0100

    Navigation

---
+++ VistivAPP/Controller/LineUpTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LineUpTableViewController.swift
+//  VistivAPP
+//
+//  Created by mobapp05 on 31/01/2019.
+//  Copyright © 2019 Jorn Swift. All rights reserved.
+//
+
+import UIKit
+
+class LineUpTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+  
+
+}
---
+++ VistivAPP/Model/Artist.swift
@@ -0,0 +1,23 @@
+//
+//  Artist.swift
+//  VistivAPP
+//
+//  Created by mobapp05 on 31/01/2019.
+//  Copyright © 2019 Jorn Swift. All rights reserved.
+//
+
+import Foundation
+
+
+class Artist{
+    
+    var naam:String
+    var uur:String
+    
+    init(naam:String, uur:String) {
+        self.naam = naam
+        self.uur = uur
+    }
+    
+}
+
